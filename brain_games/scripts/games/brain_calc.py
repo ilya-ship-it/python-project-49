7911d63e0e50b7d605a489edd3db4e5f32fe6335
ilya-ship-it <tulupov.2712@gmail.com>
2024-07-23 14:04:50 +0300

    Add brain-calc

---
+++ brain_games/scripts/games/brain_calc.py
@@ -0,0 +1,29 @@
+import prompt
+import random
+from brain_games.scripts.brain_games import main as welcome_msg
+from brain_games.scripts.engine import incorrect_answer
+
+
+def get_question():
+        operations = ['+', '-', '*']
+        operation = random.choice(operations)
+        x = random.randint(1, 100) 
+        y = random.randint(1, 100) 
+        question = (f"{x} {operation} {y}")
+        return question
+
+
+def main():
+    name = welcome_msg()
+    print('What is the result of the expression?')
+    for _ in range(3):
+        question = get_question()
+        print(f"Question: {question}")
+        answer = prompt.string('Your answer: ')
+        correct_answer = str(eval(question))  
+        if answer == correct_answer:
+            print('Correct!')
+            continue
+        else:
+            incorrect_answer(answer, correct_answer, name)
+        return
