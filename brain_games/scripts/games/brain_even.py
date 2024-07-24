b97d5d597ec47e0e8c3551a523ec0b4674fcbea4
ilya-ship-it <tulupov.2712@gmail.com>
2024-07-24 17:19:35 +0300

    Rewrite brain-even

--- brain_games/scripts/games/brain_even.py
+++ brain_games/scripts/games/brain_even.py
@@ -1,26 +1,19 @@
-import prompt
 import random
-from brain_games.scripts.brain_games import main as welcome_msg
-from brain_games.scripts.engine import is_answer_correct
-from brain_games.scripts.engine import incorrect_answer
+from brain_games.scripts.brain_games import main as ask_name
+from brain_games.scripts.engine import ask_question
 
 
-def is_even(num):
-    return num % 2 == 0
+def get_correct_answer(question):
+    game_result = question % 2 == 0
+    return 'yes' if game_result else 'no'
 
 
 def main():
-    name = welcome_msg()
+    name = ask_name()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     for _ in range(3):
         question = random.randint(1, 100)
-        print(f"Question: {question}")
-        answer = prompt.string('Your answer: ')
-        correct_answer = is_answer_correct(is_even(question))
-        if answer == correct_answer:
-            print('Correct!')
-            continue
-        else:
-            incorrect_answer(answer, correct_answer, name)
+        correct_answer = get_correct_answer(question)
+        if not ask_question(question, correct_answer, name):
             return
     print(f"Congratulations, {name}")
