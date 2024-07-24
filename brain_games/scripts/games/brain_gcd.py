bcd555a8302291dcceea8df51672ac0a8c24eb9e
ilya-ship-it <tulupov.2712@gmail.com>
2024-07-24 17:25:48 +0300

    Rewrite brain-gcd

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -1,24 +1,17 @@
-import prompt
 import random
-from brain_games.scripts.brain_games import main as welcome_msg
-from brain_games.scripts.engine import incorrect_answer
+from brain_games.scripts.brain_games import main as ask_name
+from brain_games.scripts.engine import ask_question
 from math import gcd
 
 
 def main():
-    name = welcome_msg()
+    name = ask_name()
     print('Find the greatest common divisor of given numbers.')
     for _ in range(3):
         x = random.randint(1, 100)
         y = random.randint(1, 100)
         question = (x, y)
-        print(f"Question: {question}")
-        answer = prompt.string('Your answer: ')
-        correct_answer = str(gcd(x, y))
-        if answer == correct_answer:
-            print('Correct!')
-            continue
-        else:
-            incorrect_answer(answer, correct_answer, name)
+        correct_answer = gcd(x, y)
+        if not ask_question(question, correct_answer, name):
             return
     print(f"Congratulations, {name}")
