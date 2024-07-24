dc3fd2058aeff532b28b01fe02e084ed265ea5cd
ilya-ship-it <tulupov.2712@gmail.com>
2024-07-24 18:08:14 +0300

    Add brain-prime

---
+++ brain_games/scripts/games/brain_prime.py
@@ -0,0 +1,23 @@
+import random
+from brain_games.scripts.brain_games import main as ask_name
+from brain_games.scripts.engine import ask_question
+
+
+def get_correct_answer(question):
+    if question == 1:
+        return 'no'
+    if not (question % 2 == 0 or question % 3 == 0) or (question == 2):
+        return 'yes'
+    else:
+        return 'no'
+
+
+def main():
+    name = ask_name()
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    for _ in range(3):
+        question = random.randint(1, 100)
+        correct_answer = get_correct_answer(question)
+        if not ask_question(question, correct_answer, name):
+            return
+    print(f"Congratulations, {name}")
