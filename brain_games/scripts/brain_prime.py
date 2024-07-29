2db0b055cb46dd8acb7434cfb9287c16de97b27a
ilya-ship-it <tulupov.2712@gmail.com>
2024-07-29 17:29:35 +0300

    Fix scripts

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from brain_games.engine import run_game
-import brain_games.scripts.games.brain_gcd as game
+import brain_games.scripts.games.brain_prime as game
 
 
 GAME_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from brain_games.engine import run_game
-import brain_games.scripts.games.brain_gcd as game
+import brain_games.scripts.games.brain_progression as game
 
 
 GAME_DESCRIPTION = 'What number is missing in the progression?'
